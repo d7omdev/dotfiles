6c25c27d7ea0cdec163fcb3c956aa541c1f6853a
d7omdev <abdohamdy198461@gmail.com>
2024-09-30 23:34:45 +0300

    Update launch_resolve.sh

--- executable_launch_resolve.sh
+++ executable_launch_resolve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 notify-send "Resolve Launcher" "Launching DaVinci Resolve 18" -i /home/d7om/.local/share/applications/graphics/DV_Resolve.png
-distrobox-enter Resolve-Fedora-39 -- bash -c 'export __GLX_VENDOR_LIBRARY_NAME=nvidia && exec /opt/resolve/bin/resolve'
+setsid distrobox-enter Resolve-Fedora-39 -- bash -c "__GLX_VENDOR_LIBRARY_NAME=nvidia /opt/resolve/bin/resolve" &>/dev/null 2>&1 &
