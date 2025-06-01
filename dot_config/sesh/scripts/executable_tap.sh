215740bc6633b77dd10b311fdd0f9f0f1c788b57
d7omdev <abdohamdy198461@gmail.com>
2025-06-01 22:21:31 +0300

    Remove .config/starship.toml Update .config/sesh/scripts/tap.sh Add .config/starship.toml

--- dot_config/sesh/scripts/executable_tap.sh
+++ dot_config/sesh/scripts/executable_tap.sh
@@ -6,10 +6,10 @@ FRONT_DIR="$HOME/Projects/Work/Tap/Garlik_Front/"
 # Create the "Back" window, change to the directory, and open nvim
 tmux rename-window -t "$SESSION_NAME:1" "Console"
 tmux send-keys -t "$SESSION_NAME:1" "cd $CONSOLE_DIR" C-m
-tmux send-keys -t "$SESSION_NAME:1" "nvim" C-m
+tmux send-keys -t "$SESSION_NAME:1" "nvim --listen /tmp/nvimsocket" C-m
 
 # Create the "Front" window, change to the directory, and open nvim
-tmux new-window -t "$SESSION_NAME"
-tmux rename-window -t "$SESSION_NAME:2" "Front"
-tmux send-keys -t "$SESSION_NAME:2" "cd $FRONT_DIR" C-m
-tmux send-keys -t "$SESSION_NAME:2" "nvim" C-m
+# tmux new-window -t "$SESSION_NAME"
+# tmux rename-window -t "$SESSION_NAME:2" "Front"
+# tmux send-keys -t "$SESSION_NAME:2" "cd $FRONT_DIR" C-m
+# tmux send-keys -t "$SESSION_NAME:2" "nvim" C-m
